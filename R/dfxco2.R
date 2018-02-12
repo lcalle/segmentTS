bf6f7485c876b731f45839018d1eef473e6f8c0f
lcalle <leonardo.meiji@gmail.com>
2018-02-12 11:52:29 -0700

    data description

--- R/dfxco2.R
+++ R/dfxco2.R
@@ -12,8 +12,22 @@
 #'
 #' @usage data(dfxco2)
 #'
-#' @format A data.frame object
-#'
+#' @format A data.frame with 100136 rows and 16 variables:
+#' \itemize{
+#'   \item year: year (integer)
+#'   \item month: month (integer)
+#'   \item day: day (integer)
+#'   \item func: values of the full function (harmonic+trend+short-term & long-term digital filter) (float)
+#'   \item poly: values of the polynomial part of the function (float)
+#'   \item smooth: values of the short-term smoothed curve; function + short-term filter of residualas (float)
+#'   \item trend: values of the trend curve; polynomial plus long-term filter of residuals (float)
+#'   \item detrend: values of the original data points minus the trend curve (float)
+#'   \item smcycle: values of the smoothed, detrended annual cycle; smooth - trend (float)
+#'   \item harm: values of the harmonic part of the function (float)
+#'   \item smres: values of the short-term smoothed residuals from the function (float)
+#'   \item trres: values of the long-term smoothed residual from the function (float)
+#'   \item trres: values of the growth rate; the first derivative of the trend curve (float)
+#' }
 #' @keywords datasets
 #'
 #' @references Calle, L., B. Poulter, & P.K. Patra (2018)
