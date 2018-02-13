9c03171b48019cd831243e8d122c92bf3f7fc483
lcalle <leonardo.meiji@gmail.com>
2018-02-13 12:43:05 -0700

    rename output return

--- R/segmentTS.4segdist.R
+++ R/segmentTS.4segdist.R
@@ -56,8 +56,8 @@ segmentTS.4segdist <- function(obs.seg, sim.seg){
   #names(ls.diff) = c('dist_tdiff', 'dist_vdiff', 'poly_t', 'poly_v')
   return(list(dist_tdiff= dist_tdiff,
 			  dist_vdiff= dist_vdiff,
-			   poly_tsim= poly_tsim,
-			   poly_vsim= poly_vsim,
-			   poly_tobs= poly_tobs,
-               poly_vobs= poly_vobs))
+			  poly_t.sim= poly_t.sim,
+			  poly_v.sim= poly_v.sim,
+			  poly_t.obs= poly_t.obs,
+              poly_v.obs= poly_v.obs))
 } 
